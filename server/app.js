e318e3fb17cd3910ab3f035e99208942a1893b83
Sasha Bespalko <a.bespalko88@gmail.com>
2018-06-23 13:51:27 +0300

    fourth commit

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use( bodyParser.json() );
 
 app.use(cors({ origin: '*' }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../public/index.html'));
